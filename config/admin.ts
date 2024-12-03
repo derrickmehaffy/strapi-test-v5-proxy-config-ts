31cb772de3aa781aae1893d29a44708692fe8563
Derrick Mehaffy <derrickmehaffy@gmail.com>
2024-12-03 11:19:53 -0800

    init

--- config/admin.ts
+++ config/admin.ts
@@ -1,17 +1,44 @@
+import { Strategy as DiscordStrategy } from "passport-discord";
+
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env("ADMIN_JWT_SECRET"),
+    providers: [
+      {
+        uid: "discord",
+        displayName: "Discord",
+        icon: "https://cdn0.iconfinder.com/data/icons/free-social-media-set/24/discord-512.png",
+        createStrategy: (strapi) =>
+          new DiscordStrategy(
+            {
+              clientID: env("DISCORD_CLIENT_ID"),
+              clientSecret: env("DISCORD_SECRET"),
+              callbackURL:
+                strapi.admin.services.passport.getStrategyCallbackURL(
+                  "discord"
+                ),
+              scope: ["identify", "email"],
+            },
+            (accessToken, refreshToken, profile, done) => {
+              done(null, {
+                email: profile.email,
+                username: `${profile.username}`,
+              });
+            }
+          ),
+      },
+    ],
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env("API_TOKEN_SALT"),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env("TRANSFER_TOKEN_SALT"),
     },
   },
   flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+    nps: env.bool("FLAG_NPS", true),
+    promoteEE: env.bool("FLAG_PROMOTE_EE", true),
   },
 });
